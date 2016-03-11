48ca350926b9416b27f8541fa15797de03a6eb28
C Wen <whywe@live.com>
2016-03-10 19:40:52 -0500

    accumulated update til 3/10

--- calculate_gradient.m
+++ calculate_gradient.m
@@ -1,4 +1,4 @@
-function D = calculate_gradient(cp,pd,F)
+function D = calculate_gradient(cp,pd,sigma)
 % gradient is the area of cells, parts inside the given polygon cp
 nc = size(pd.cell,1);
 D = zeros(nc,1);
@@ -14,17 +14,35 @@ for i = 1:nc
 end
 cp(end,:) = [];
 
+need_more_accurate = false;
 for i = 1:nc
     ci = pd.dpe(pd.cell{i},:);
-%     ci = flipud(ci);    
+    
+    if need_more_accurate
+        if ~in(i)
+            pc = polybool([cp(:,1),cp(:,2)],[ci(:,1),ci(:,2)],'and');
+            ci = pc{1};
+            ci = ci([1:end,1],:);
+        end
+        
+        p0 = mean(ci(1:end-1,:));
+
+        for j = 1:length(ci)-1
+            p1 = ci(j,:);
+            p2 = ci(j+1,:);
+            D(i) = D(i) + face_area([1,2,3],[p0;p1;p2])*sigma(mean([p0;p1;p2]));
+        end
+    end
+    
     if in(i)
-        mui = (2*mean(F(ci))+F(mean(ci)))/3;
+        ci = ci(1:end-1,:);
+        mui = (2*mean(sigma(ci))+sigma(mean(ci)))/3;
         D(i) = polyarea(ci(:,1),ci(:,2))*mui;        
     else % if cell's part outside cp
         try
             pc = polybool([cp(:,1),cp(:,2)],[ci(:,1),ci(:,2)],'and');
             xy = pc{1};
-            mui = (2*mean(F(xy))+F(mean(xy)))/3;
+            mui = (2*mean(sigma(xy))+sigma(mean(xy)))/3;
             D(i) = polyarea(xy(:,1),xy(:,2))*mui;
         catch ex
             save gradient
